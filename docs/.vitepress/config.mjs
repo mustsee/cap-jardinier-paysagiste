73d9f441ced6ccf05a9ac455dc0feb7f9559fa7f
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2025-09-24 17:48:21 +0200

    build again

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/cap-jardinier-paysagiste/',
   title: "CAPA Jardinier-paysagiste",
   description: "2025-2026",
   themeConfig: {
